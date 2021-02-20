bd24883bef349f542454d58132a617ff38cf08d3
raduedi <61558309+raduedi@users.noreply.github.com>
2021-02-20 13:21:15 +0100

    Add files via upload

---
+++ BasicCalculator.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class BasicCalculator {
+    private double firstNumber;
+    private double secondNumber;
+
+    public BasicCalculator(double firstNumber, double secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public void getAdditionResult() {
+        System.out.println("The sum result is  : " + (firstNumber + secondNumber));
+    }
+
+    public void getSubtractionResult() {
+        System.out.println("The subtraction result is : " + (firstNumber - secondNumber));
+    }
+
+    public void getMultiplicationResult() {
+        System.out.println("The multiplication result is : " + (firstNumber * secondNumber));
+    }
+
+    public void getDivisionResult() {
+        if (secondNumber == 0) {
+            System.out.println("The division result is :" + 0 );
+        } else {
+            System.out.println("The division result is  : " + (firstNumber/secondNumber));
+        }
+    }
+}
